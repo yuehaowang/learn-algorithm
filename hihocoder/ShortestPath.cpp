f4f0d969c53aa3d35db76f9f18f663b9542e9ad0
yuehaowang <wangyuehao1999@gmail.com>
2018-11-20 00:33:50 +0800

    AC shortest path.

--- hihocoder/ShortestPath.cpp
+++ hihocoder/ShortestPath.cpp
@@ -20,25 +20,53 @@ struct node {
 };
 
 
-void Dijkstra(int N, node** edges, int* path_len, int* parent)
+void Dijkstra(int N, node** edges, int* path_len)
 {
 	int unvisited[N + 1];
-	int unvisited_num = N + 1;
+	int unvisited_num = N;
+
+	for (int i = 0; i <= N; i++) {
+		unvisited[i] = 0;
+	}
 
 	while (unvisited_num > 0) {
 		// find a vertex with the minimum length from the start
 		int mini, mini_dist = INF;
 
-		for (int i = 0; i < N+1; i++) {
-			if (path_len[i] < mini_dist) {
+		for (int i = 0; i <= N; i++) {
+			if (unvisited[i] == 0 && path_len[i] < mini_dist) {
 				mini = i;
+				mini_dist = path_len[i];
 			}
 		}
 
+		unvisited[mini] = 1;
+		unvisited_num--;
+
 		// re-calculate the length of the vertex's adjacent vertices
+		node* next = edges[mini] -> next;
+
+		while(next != nullptr) {
+			if (unvisited[next -> val] == 0) {
+				int d = mini_dist + next -> w;
+
+				if (d < path_len[next -> val]) {
+					path_len[next -> val] = d;
+				}
+			}
+
+			next = next -> next;
+		}
 	}
 }
 
+void add_edge(node** edges, int u, int v, int l)
+{
+	node* n = new node(v);
+	n -> next = edges[u] -> next;
+	n -> w = l;
+	edges[u] -> next = n;
+}
 
 int main()
 {
@@ -47,11 +75,9 @@ int main()
 	cin >> N >> M >> S >> T;
 
 	node* edges[N + 1];
-	int path_len[N + 1];
-	int parent[N + 1];
-	
+	int path_len[N + 1];	
 
-	for (int v = 0; v < N; v++) {
+	for (int v = 0; v <= N; v++) {
 		node* head = new node(v);
 		edges[v] = head;
 
@@ -67,14 +93,13 @@ int main()
 
 		cin >> u >> v >> l;
 
-		node* head = new node*(v);
-		head -> next = edges[u] -> next;
-		head -> w = l;
-		edges[u] -> next = head;
+		add_edge(edges, u, v, l);
+		add_edge(edges, v, u, l);
 	}
 
-	Dijkstra(N, edges, path_len, parent);
+	Dijkstra(N, edges, path_len);
 
+	cout << path_len[T] << endl;
 	
 	return 0;
 }
