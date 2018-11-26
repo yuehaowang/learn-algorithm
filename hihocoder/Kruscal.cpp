3c500cd4c7b60a139e311eecca15360e05f985b2
yuehaowang <wangyuehao1999@gmail.com>
2018-11-26 12:38:54 +0800

    AC Kruscal.

--- hihocoder/Kruscal.cpp
+++ hihocoder/Kruscal.cpp
@@ -1,3 +1,8 @@
+/************
+ * http://hihocoder.com/problemset/problem/1098
+ */
+
+
 #include <iostream>
 
 using namespace std;
@@ -23,6 +28,7 @@ struct edge
 class MinHeap
 {
 private:
+
 	edge** array;
 	int tail;
 	int maxNum;
@@ -78,6 +84,7 @@ private:
 	}
 
 public:
+
 	MinHeap(int N) : tail(0), maxNum(N)
 	{
 		array = new edge*[N + 1];
@@ -87,7 +94,7 @@ public:
 		} 
 	}
 
-	~MinHeap()
+	virtual ~MinHeap()
 	{
 		for (int i = 1; i <= maxNum; i++) {
 			if (array[i] != nullptr) {
@@ -95,7 +102,7 @@ public:
 			}
 		}
 
-		delete array;
+		delete[] array;
 	}
 
 	void push(int v1, int v2, int w)
@@ -126,8 +133,61 @@ public:
 	{
 		return tail == 0;
 	}
+
 };
 
+
+
+/**
+ * UnionSet
+ */
+
+class UnionFind
+{
+private:
+
+	int* set;
+
+public:
+
+	void unite(int n, int m)
+	{
+		int n_root = find(n);
+		int m_root = find(m);
+
+		if (n_root > m_root) {
+			int temp = m_root;
+			m_root = n_root;
+			n_root = temp;
+		}
+
+		set[m_root] = n_root;
+	}
+
+	int find(int ele)
+	{
+		if (set[ele] != ele) {
+			set[ele] = find(set[ele]);
+		}
+
+		return set[ele];
+	}
+
+	UnionFind(int N)
+	{
+		set = new int[N + 1];
+		
+		for (int i = 0; i <= N; i++) {
+			set[i] = i;
+		}
+	}
+
+	virtual ~UnionFind()
+	{
+		delete[] set;
+	}
+
+};
 
 
 /**
@@ -136,16 +196,37 @@ public:
 
 int main()
 {
-	MinHeap h(3);
-	h.push(1, 1, 0);
-	h.push(1, 2, 5);
-	h.push(1, 6, 4);
+	int N, M;
+	cin >> N >> M;
 
-	edge e1 = h.pop();
-	cout << e1.w << endl;
-	edge e2 = h.pop();
-	cout << e2.w << endl;
-	edge e3 = h.pop();
-	cout << e3.w << endl;
+	MinHeap p_queue(M);
+	UnionFind uf_set(N);
+
+	for (int i = 0; i < M; ++i){
+		int n1, n2, w;
+
+		cin >> n1 >> n2 >> w;
+
+		p_queue.push(n1, n2, w);
+	}
+
+	long long sum = 0;
+	int added_edge = 0;
+
+	/** Kruscal */
+	while (added_edge < N + 1 && !p_queue.empty()) {
+		edge min_e = p_queue.pop();
+
+		if (uf_set.find(min_e.v1) != uf_set.find(min_e.v2)) {
+			sum += min_e.w;
+
+			added_edge++;
+
+			uf_set.unite(min_e.v1, min_e.v2);
+		}
+	}
+
+	cout << sum << endl;
+	
 	return 0;
 }
